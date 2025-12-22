2ef7e97fe246b9da0b294cab4af12370b2b74eaf
Pratik Potdukhe <63575398+pratikpotdukhe30@users.noreply.github.com>
2025-12-22 16:56:47 +0100

    Added logic for same code without textwrap library and using string slicing

--- textwrap.py
+++ textwrap.py
@@ -8,3 +8,19 @@ if __name__ == '__main__':
     string, max_width = input(), int(input())
     result = wrap(string, max_width)
     print(result)
+
+
+#Same code without textwrap library and own logic -
+
+import textwrap
+
+def wrap(string, max_width):
+    finalStr = ""
+    for i in range(0, len(string), max_width):
+        finalStr += string[i:i+max_width] + "\n"
+    
+    return finalStr
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
