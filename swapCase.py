9ced90e719eec2bea2db7398c7fc8becc0830cd5
Pratik Potdukhe <63575398+pratikpotdukhe30@users.noreply.github.com>
2025-12-28 22:31:41 +0100

    Implement manual swap_case function
    Added a manual implementation of swap_case function.

--- swapCase.py
+++ swapCase.py
@@ -5,3 +5,16 @@ if __name__ == '__main__':
     s = input()
     result = swap_case(s)
     print(result)
+
+
+def swap_case_manual(s):
+    result = ""
+    for char in s:
+        if char.islower():
+            result += char.upper()
+        elif char.isupper():
+            result += char.lower()
+        else:
+            result += char    
+            
+    return result
